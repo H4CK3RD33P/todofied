7273889b86f6b3c3be61b5321d014bfc3c04d8bb
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2020-12-26 23:42:54 +0530

    Added registration form in the sign-up page.
    I displayed Django's built in user sign-up form in the sign-up page and added minimal functionality to it, like bypassing
    Django's CSRF mechanism and successfully perform a POST.

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm # this will include Django's built-in sign-up form template class.
 # Create your views here.
 def signupuser(request):
-    return render(request,'todo/signupuser.html')
+    #We are creating an instance of UserCreationForm (sign-up form) and passing it to the template
+    return render(request,'todo/signupuser.html',{'form': UserCreationForm()})
