4593b20421949087399acbc37968d19f04862726
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2020-12-28 22:24:18 +0530

    Added the way to handle post request from signup page.
    Now signupuser() will handle GET and POST request differently.
    User objects are created and saved in the database if the two passwords the user provides matches.
    However, if it doesn't, it displays an error message in the terminal.
    After submitting the form, the user will be saved in the database but Django will throw error since we don't actually provide a webpage to show after the action taken.

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm # this will include Django's built-in sign-up form template class.
+from django.contrib.auth.models import User #this will import the built-in User model of Django
 # Create your views here.
-def signupuser(request):
-    #We are creating an instance of UserCreationForm (sign-up form) and passing it to the template
-    return render(request,'todo/signupuser.html',{'form': UserCreationForm()})
+def signupuser(request): 
+    if request.method == "GET": #this will happen when a user opens the signup/ path 
+        #We are creating an instance of UserCreationForm (sign-up form) and passing it to the template
+        return render(request,'todo/signupuser.html',{'form': UserCreationForm()})
+    else: #this will happen when user submits the form
+        if request.POST['password1'] == request.POST['password2']: #only if the rewritten password matches the previous password
+            user = User.objects.create_user(username=request.POST['username'],password=request.POST['password1']) #create a User object with the given username and password
+            user.save() #save the user object in the database
+        else: #if the two passwords do not match
+            print("Error: Password not matched.")
