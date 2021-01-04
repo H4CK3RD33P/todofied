d55e56e2226778d6949ad3c5107261e1c25e1bc9
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2021-01-04 22:14:34 +0530

    Created TodoAdmin class and registered it with admin.
    Created TodoAdmin to display the 'created' field of the 'Todo' model in the admin field.

--- todo/admin.py
+++ todo/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Todo
 # Register your models here.
-admin.site.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',) #this line shows up the uneditable field 'created' of the Todo model IN THE ADMIN PANEL. However, this is 'read-only' and cannot be edited.
+
+admin.site.register(Todo,TodoAdmin)
